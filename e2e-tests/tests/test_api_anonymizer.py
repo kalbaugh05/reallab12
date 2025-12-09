3c3275ef857c3f62b6032a0cde9e3c91ca56dc08
Kyra Albaugh <231558924+kalbaugh05@users.noreply.github.com>
2025-12-09 05:07:26 +0000

    Add /genz-preview endpoint

--- e2e-tests/tests/test_api_anonymizer.py
+++ e2e-tests/tests/test_api_anonymizer.py
@@ -70,6 +70,7 @@ def test_given_anonymize_called_with_empty_analyzer_results_then_unchanged_text_
             "PHONE_NUMBER": { "type": "mask", "masking_char": "*", "chars_to_mask": 4, "from_end": true }
         },
         "analyzer_results": [
+
         ]
     }
     """
@@ -400,3 +401,29 @@ def test_overlapping_keep_both():
 
     assert response_status == 200
     assert equal_json_strings(expected_response, response_content)
+
+
+@pytest.mark.api
+def test_given_anonymize_called_with_genz_then_expected_valid_response_returned():
+    payload = {
+        "text": "Please contact Emily Carter at 734-555-9284 if you have questions about the workshop registration.",
+        "analyzer_results": [
+            {
+                "start": 15,
+                "end": 27,
+                "score": 0.3,
+                "entity_type": "PERSON"
+            },
+            {
+                "start": 31,
+                "end": 43,
+                "score": 0.95,
+                "entity_type": "PHONE_NUMBER"
+            }
+        ]
+    }
+
+    status, _ = genz(payload)
+
+    # Only checking status code because GenZ operator output is random
+    assert status == 200
